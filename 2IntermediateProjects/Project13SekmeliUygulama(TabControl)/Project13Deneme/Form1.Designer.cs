fd38f65d617187e4b91d4647af5cc7d05a7e4777
MehdiBurakErdem <mehdierdem7@gmail.com>
2025-11-23 21:32:59 +0300

    Add Project13SekmeliUygulama(TabControl)

---
+++ 2IntermediateProjects/Project13SekmeliUygulama(TabControl)/Project13Deneme/Form1.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace Project13Deneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.SendingSituation = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cbSituationNo = new System.Windows.Forms.CheckBox();
+            this.cbSituationOK = new System.Windows.Forms.CheckBox();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.SubmitB = new System.Windows.Forms.Button();
+            this.bttnColorDialog = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbCountry = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SurnameTextBox = new System.Windows.Forms.TextBox();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 19);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(814, 787);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.SendingSituation);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.cbSituationNo);
+            this.tabPage1.Controls.Add(this.cbSituationOK);
+            this.tabPage1.Controls.Add(this.rbFemale);
+            this.tabPage1.Controls.Add(this.rbMale);
+            this.tabPage1.Controls.Add(this.SubmitB);
+            this.tabPage1.Controls.Add(this.bttnColorDialog);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.cmbCountry);
+            this.tabPage1.Controls.Add(this.dateTimePicker1);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.SurnameTextBox);
+            this.tabPage1.Controls.Add(this.NameTextBox);
+            this.tabPage1.Controls.Add(this.progressBar1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(806, 754);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Registration";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // SendingSituation
+            // 
+            this.SendingSituation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.SendingSituation.Location = new System.Drawing.Point(145, 590);
+            this.SendingSituation.Name = "SendingSituation";
+            this.SendingSituation.Size = new System.Drawing.Size(443, 41);
+            this.SendingSituation.TabIndex = 19;
+            this.SendingSituation.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(24, 419);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(133, 41);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "I am";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cbSituationNo
+            // 
+            this.cbSituationNo.AutoSize = true;
+            this.cbSituationNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cbSituationNo.Location = new System.Drawing.Point(321, 430);
+            this.cbSituationNo.Name = "cbSituationNo";
+            this.cbSituationNo.Size = new System.Drawing.Size(99, 26);
+            this.cbSituationNo.TabIndex = 17;
+            this.cbSituationNo.Text = "Upset :(";
+            this.cbSituationNo.UseVisualStyleBackColor = true;
+            // 
+            // cbSituationOK
+            // 
+            this.cbSituationOK.AutoSize = true;
+            this.cbSituationOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cbSituationOK.Location = new System.Drawing.Point(187, 430);
+            this.cbSituationOK.Name = "cbSituationOK";
+            this.cbSituationOK.Size = new System.Drawing.Size(78, 26);
+            this.cbSituationOK.TabIndex = 16;
+            this.cbSituationOK.Text = "OK :)";
+            this.cbSituationOK.UseVisualStyleBackColor = true;
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbFemale.Location = new System.Drawing.Point(321, 378);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(94, 26);
+            this.rbFemale.TabIndex = 15;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbMale.Location = new System.Drawing.Point(187, 378);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(73, 26);
+            this.rbMale.TabIndex = 14;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = true;
+            // 
+            // SubmitB
+            // 
+            this.SubmitB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.SubmitB.Location = new System.Drawing.Point(321, 504);
+            this.SubmitB.Name = "SubmitB";
+            this.SubmitB.Size = new System.Drawing.Size(145, 56);
+            this.SubmitB.TabIndex = 12;
+            this.SubmitB.Text = "Submit";
+            this.SubmitB.UseVisualStyleBackColor = true;
+            this.SubmitB.Click += new System.EventHandler(this.SubmitB_Click);
+            // 
+            // bttnColorDialog
+            // 
+            this.bttnColorDialog.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bttnColorDialog.Location = new System.Drawing.Point(174, 301);
+            this.bttnColorDialog.Name = "bttnColorDialog";
+            this.bttnColorDialog.Size = new System.Drawing.Size(91, 42);
+            this.bttnColorDialog.TabIndex = 11;
+            this.bttnColorDialog.Text = "Select";
+            this.bttnColorDialog.UseVisualStyleBackColor = true;
+            this.bttnColorDialog.Click += new System.EventHandler(this.bttnColorDialog_Click);
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(24, 368);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(133, 41);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Gender";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(24, 301);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 41);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Your Color";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cmbCountry
+            // 
+            this.cmbCountry.FormattingEnabled = true;
+            this.cmbCountry.Items.AddRange(new object[] {
+            "Turkey",
+            "Germany",
+            "Chinese",
+            "United States",
+            "Avustralia"});
+            this.cmbCountry.Location = new System.Drawing.Point(187, 244);
+            this.cmbCountry.Name = "cmbCountry";
+            this.cmbCountry.Size = new System.Drawing.Size(200, 28);
+            this.cmbCountry.TabIndex = 8;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(187, 180);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(24, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 41);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Country";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(24, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 41);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Birthday";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(24, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 41);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Surname";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SurnameTextBox
+            // 
+            this.SurnameTextBox.Location = new System.Drawing.Point(187, 91);
+            this.SurnameTextBox.Name = "SurnameTextBox";
+            this.SurnameTextBox.Size = new System.Drawing.Size(209, 26);
+            this.SurnameTextBox.TabIndex = 2;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(187, 50);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(209, 26);
+            this.NameTextBox.TabIndex = 3;
+            this.NameTextBox.TextChanged += new System.EventHandler(this.NameTextBox_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(145, 653);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(443, 65);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Value = 15;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(24, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.listBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(806, 754);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Show Information";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(153, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(446, 43);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Your Information";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(157, 124);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(442, 344);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.Color = System.Drawing.Color.White;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 818);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox SurnameTextBox;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button SubmitB;
+        private System.Windows.Forms.Button bttnColorDialog;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbCountry;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox cbSituationNo;
+        private System.Windows.Forms.CheckBox cbSituationOK;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.Label SendingSituation;
+    }
+}
+
---
+++ 2IntermediateProjects/Project13SekmeliUygulama(TabControl)/Project13Deneme/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Project13Deneme
+{
+    public partial class Form1 : Form
+    {
+        ColorDialog color = new ColorDialog();
+        public Form1()
+        {
+            InitializeComponent();
+            color.Color = Color.White;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SubmitB_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 10;
+            SendingSituation.Text = "";
+
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Name: " + NameTextBox.Text);
+            listBox1.Items.Add("Surname: " + SurnameTextBox.Text);
+            listBox1.Items.Add("Age: " + (DateTime.Now.Year - dateTimePicker1.Value.Year));
+
+            listBox1.Items.Add("Your country is: " + cmbCountry.Text);
+            if (rbMale.Checked) { listBox1.Items.Add("Person is Okay"); } else {listBox1.Items.Add("Person is not Okay"); }
+            
+            if(cbSituationOK.Checked && !(cbSituationNo.Checked)) { listBox1.Items.Add("Statuation is OK"); } 
+            else if (!(cbSituationOK.Checked) && cbSituationNo.Checked) { listBox1.Items.Add("Statuation is not OK"); }
+            else { listBox1.Items.Add("Statuation is not defined"); }
+
+            for (int i = 10; i <= 100; i++)
+                progressBar1.Value = i;
+
+            if (progressBar1.Value == 100)
+                SendingSituation.Text = "Information Sent... You can check information page";
+
+            if (color != null)
+            {
+                listBox1.BackColor = color.Color;
+                color.Reset();
+            }
+
+            tabPage1.Refresh();
+        }
+
+        private void NameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bttnColorDialog_Click(object sender, EventArgs e)
+        {
+            color.ShowDialog();
+        }
+    }
+}
